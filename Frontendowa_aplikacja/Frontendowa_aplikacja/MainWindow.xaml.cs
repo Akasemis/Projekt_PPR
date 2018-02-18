8bd71cb34a821f52eb446dab053e57c4dfc2472e
Marek Godlewski <Akasemis@gmail.com>
2018-02-18 19:05:45 +0100

    Dodano buttony

--- Frontendowa_aplikacja/Frontendowa_aplikacja/MainWindow.xaml.cs
+++ Frontendowa_aplikacja/Frontendowa_aplikacja/MainWindow.xaml.cs
@@ -20,15 +20,59 @@ namespace Frontendowa_aplikacja
     /// </summary>
     public partial class MainWindow : Window
     {
+        int stan = 1;
         public MainWindow()
         {
             InitializeComponent();
             var serwer = new ServiceReference1.Service1Client();
-            ile.Content = serwer.ile_wierszy();
-            imie.Content += serwer.zwroc_imie(1);
-            rasa.Content += serwer.zwroc_rase(1);
-            wiek.Content += Convert.ToString(serwer.zwroc_wiek(1));
-            kontakt.Content += serwer.zwroc_kontakt(1);
+            imie_t.Content = serwer.zwroc_imie(1);
+            rasa_t.Content = serwer.zwroc_rase(1);
+            wiek_t.Content = Convert.ToString(serwer.zwroc_wiek(1));
+            kontakt_t.Content = serwer.zwroc_kontakt(1);
+        }
+
+        private void zwierzak_next(object sender, RoutedEventArgs e)
+        {
+            var serwer = new ServiceReference1.Service1Client();
+            int ile = serwer.ile_wierszy();
+            if (stan < ile)
+            {
+                stan++;
+                imie_t.Content = serwer.zwroc_imie(stan);
+                rasa_t.Content = serwer.zwroc_rase(stan);
+                wiek_t.Content = Convert.ToString(serwer.zwroc_wiek(stan));
+                kontakt_t.Content = serwer.zwroc_kontakt(stan);
+            }
+            else
+            {
+                stan = 1;
+                imie_t.Content = serwer.zwroc_imie(stan);
+                rasa_t.Content = serwer.zwroc_rase(stan);
+                wiek_t.Content = Convert.ToString(serwer.zwroc_wiek(stan));
+                kontakt_t.Content = serwer.zwroc_kontakt(stan);
+            }
+        }
+
+        private void zwierzak_prev(object sender, RoutedEventArgs e)
+        {
+            var serwer = new ServiceReference1.Service1Client();
+            int ile = serwer.ile_wierszy();
+            if (stan > 1)
+            {
+                stan--;
+                imie_t.Content = serwer.zwroc_imie(stan);
+                rasa_t.Content = serwer.zwroc_rase(stan);
+                wiek_t.Content = Convert.ToString(serwer.zwroc_wiek(stan));
+                kontakt_t.Content = serwer.zwroc_kontakt(stan);
+            }
+            else
+            {
+                stan = serwer.ile_wierszy();
+                imie_t.Content = serwer.zwroc_imie(stan);
+                rasa_t.Content = serwer.zwroc_rase(stan);
+                wiek_t.Content = Convert.ToString(serwer.zwroc_wiek(stan));
+                kontakt_t.Content = serwer.zwroc_kontakt(stan);
+            }
         }
     }
 }
